416ed6a96f0d38570d625bfa7d4c7bdd2dcf0a30
Vincenzo <vinzgameslol@gmail.com>
2020-02-12 11:45:25 +0100

    Finished script for color and button

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-let gridContainer = document.querySelector(".grid-container")
-let btnPrompt;
+let gridContainer = document.querySelector(".grid-container");
+
+let btnPrompt 
+
+let howManyPixels;
 
 function pixelsPerPage(){
-  let screenArea = window.outerWidth * window.outerHeight;
-  let pixelArea = btnPrompt ** 2;
-  let howManyPixels = (screenArea / pixelArea);
-  return howManyPixels
+screenWidth = window.outerWidth;
+screenHeight = window.outerHeight;  
+screenArea = screenWidth * screenHeight;
+pixelArea = btnPrompt ** 2;
+howManyPixels = screenArea / pixelArea;
 }
 
 function createDivs(){
-    for(let i = 0; i < pixelsPerPage(); i++){
+  while(gridContainer.firstChild){
+    gridContainer.removeChild(gridContainer.firstChild)
+  }
+    for(let i = 0; i < howManyPixels; i++){
       let squareDivs = document.createElement('div');
       squareDivs.classList.add('squareDivs')
-      squareDivs.style.cssText ="float:right;margin:0px";
-      squareDivs.style.width = btnPrompt;
-      squareDivs.style.height = btnPrompt;
+      squareDivs.style.cssFloat = "right"
+      squareDivs.style.margin = "0px"
+      squareDivs.style.width = btnPrompt + "px";
+      squareDivs.style.height = btnPrompt + "px";
       gridContainer.appendChild(squareDivs);
     }
+
+let toBlack = 1;
+
     [...gridContainer.children].forEach(child => {
          child.addEventListener("mouseover", (e) => {
-           child.style.backgroundColor = "black";
+          toBlack -= 0.1 ; 
+          let r = Math.floor((Math.random()*(255 + 1)));
+          let g = Math.floor((Math.random()*(255 + 1)));
+          let b = Math.floor((Math.random()*(255 + 1)));
+           child.style.backgroundColor = "rgb(" +(r * toBlack) + "," + (g * toBlack) + "," + (b * toBlack) +")"
          })
     })
   document.body.appendChild(gridContainer)
 }
+
 let pixelSize;
+
 
 function changeSize(){
   btnPrompt = prompt("Choose the size of the brush", "10");
@@ -32,6 +49,3 @@ function changeSize(){
   pixelsPerPage();
   createDivs();
 };
-
-
-document.getElementById("sizebtn").addEventListener("click", changeSize())
